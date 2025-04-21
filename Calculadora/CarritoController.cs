8dc5195c5fac81daaea5586f8cd659ab8ebfb9ed
aaron acuña <aaronacu_85@hotmail.com>
2025-04-20 23:35:51 -0500

    carrito controller agregado.

---
+++ Calculadora/CarritoController.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculadora
+{
+    public class CarritoController
+    {
+        private readonly ICarrito _carrito;
+
+        public CarritoController(ICarrito carrito) => _carrito = carrito;
+
+        public IEnumerable<Producto> ObtenerProductos()
+            => _carrito.ObtenerProductos();
+
+        public void AgregarProducto
+            (string nombre, double precio)
+            => _carrito.AgregarProducto(nombre, precio);
+
+        public double CalcularMontoFinal()
+            => _carrito.CalcularMontoFinal();
+    }
+}
